42a08af18ad6837eb63e897ae67972b415ba60c5
Veros Kaplan <veros@koren.cz>
2014-07-03 12:07:24 +0200

    Tested mount to full names

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,36 +6,21 @@ package{'xfsprogs':
   ensure => present
 }
 
-# create mountpoints
-file{'/srv/test1':
-  ensure => 'directory'
-}
-file{'/srv/test2':
-  ensure => 'directory'
+file {'/srv/backup':
+  ensure => directory,
 }
 
+filesystem { '/dev/disk/by-id/scsi-SATA_VIRTUAL_second':
+  ensure  => present,
+  fs_type => 'xfs',
+  require => Package['xfsprogs'],
+}
 
-class {'lvm':
-    volume_groups => {
-      'second' => {
-        physical_volumes => ['/dev/disk/by-id/scsi-SATA_VIRTUAL_second'],
-        logical_volumes => {
-          'test1' => {
-            size => '128M',
-            fs_type => 'xfs',
-            mountpath => '/srv/test1',
-            mountpath_require => true,
-
-          },
-          'test2' => {
-            size => '128M',
-            fs_type => 'ext3',
-            mountpath => '/srv/test2',
-            mountpath_require => false,
-          },
-        }
-      }
-    },
-
-    require => Package['xfsprogs']
-  }
+mount {'/srv/backup':
+  device => '/dev/disk/by-id/scsi-SATA_VIRTUAL_second',
+  fstype => 'xfs',
+  ensure => mounted,
+  atboot => true,
+  options => 'auto',
+  require => Filesystem['/dev/disk/by-id/scsi-SATA_VIRTUAL_second'],
+}
