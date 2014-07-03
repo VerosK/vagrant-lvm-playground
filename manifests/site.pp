95718394fd0ebd5027d5be84399a4e2624cad246
Veros Kaplan <veros@koren.cz>
2014-07-03 23:10:59 +0200

    Moved creation of filesystem to one wrapper.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,26 +1,53 @@
 
 notify{"Started on  ${fqdn}": }
 
-# install XFS programsfac
-package{'xfsprogs':
-  ensure => present
+# Mount filesystem from $blockdevice to $mountpoint
+# Create filesystem $fs_type if needed
+# Create Icinga check
+define foo_filesystem(
+      $blockdevice,
+      $mountpoint = $title,
+      $fs_type = 'ext3',
+    ) {
+
+  # install mkfs.xfs if needed
+  if $fs_type == 'xfs' {
+    case $operatingsystem {
+      'centos','redhat' : { $fs_package = 'xfsprogs' }
+      'ubuntu': { $fs_package = 'xfsprogs' }
+      default: { $fs_package = undef }
+    }
+  }
+
+  if $fs_package and (!defined(Package[$fs_package])) {
+    package {$fs_package:
+      ensure => present,
+      before => Filesystem[$blockdevice]
+    }
+  }
+
+  file { $mountpoint:
+    ensure => directory,
+    before => Mount[$mountpoint],
+  }
+
+  filesystem { $blockdevice:
+    ensure  => present,
+    fs_type => $fs_type,
+  }
+
+r
+  mount {$mountpoint:
+    ensure  => mounted,
+    device  => $blockdevice,
+    fstype  => $fs_type,
+    atboot  => true,
+    options => 'auto',
+    require => Filesystem[$blockdevice],
+  }
 }
 
-file {'/srv/backup':
-  ensure => directory,
-}
-
-filesystem { '/dev/disk/by-id/scsi-SATA_VIRTUAL_second':
-  ensure  => present,
-  fs_type => 'xfs',
-  require => Package['xfsprogs'],
-}
-
-mount {'/srv/backup':
-  device => '/dev/disk/by-id/scsi-SATA_VIRTUAL_second',
-  fstype => 'xfs',
-  ensure => mounted,
-  atboot => true,
-  options => 'auto',
-  require => Filesystem['/dev/disk/by-id/scsi-SATA_VIRTUAL_second'],
+foo_filesystem{'/srv/backup/':
+  blockdevice => '/dev/disk/by-id/scsi-SATA_VIRTUAL_second',
+  fs_type     => 'xfs',
 }
